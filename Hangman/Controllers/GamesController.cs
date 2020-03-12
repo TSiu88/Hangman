db225dc75b4ee15955c235fbdd336b912f93b42b
Tiffany Siu <tsiu88@gmail.com>
2020-03-12 16:47:47 -0700

    Add view and controller functionality, unfinished currently
    Co-authored-by: Jiwon Han <jiwon1.han@gmail.com>

--- Hangman/Controllers/GamesController.cs
+++ Hangman/Controllers/GamesController.cs
@@ -6,11 +6,13 @@ namespace Hangman.Controllers
 {
   public class GamesController : Controller
   {
-    [HttpGet("/Games")]
+    [HttpGet("/games")]
     public ActionResult Index() 
     { 
-      List<Game> allGames = Game.GetAll();
-      return View(allGames);
+      List<char> allGuessedLetters = Game.GetAllGuessedLetters();
+
+      
+      return View(allGuessedLetters);
     }
 
     [HttpGet("/games/new")]
@@ -19,8 +21,53 @@ namespace Hangman.Controllers
     [HttpPost("/games")]
     public ActionResult Create(char guess) 
     { 
+      Dictionary<string, int> dict = new Dictionary<string, int>();
       
-      return RedirectToAction("Index"); 
+  //     for(int i = 0; i < Game.WordToGuess.Length; i++)
+  //     {
+  //     if(Game.AlreadyGuessed(guess) == false)
+  //     {
+  //       // dict.Add("gameOver", "true");
+  //       Game.WrongGuesses ++;
+  //     }
+  //     else if(Game.WordToGuess[i] == guess)
+  //     {
+  //       Game.Rightguesses++;
+  //     }
+  //     else if(Game.WordToGuess[i] == guess)
+  //     {
+  //       Game.WrongGuesses ++;
+  //       Game.MatchedChar;
+  //     }
+  //     }
+  //     dict.Add("wrongguess", Game.WrongGuesses);
+  //     dict.Add("Rightguesses", Game.Rightguesses);
+
+  //     if(Game.WrongGuesses >= 6)
+  //     {
+  //       Game.ClearAllGuesses();
+  //       dict("gameover", "true");
+  //     }
+  //     else if(Game.WordToGuess.Length == Game.Rightguesses)
+  //     {
+  //       dict("gameover", "false");
+  //     }
+
+  // trying to figure out...!
+      // else if(Game.GuessLetterCorrectly(guess) == false)
+      // {
+      //   Game.MatchCharValidator(guess);
+      //   wrongguess ++;
+      // } 
+      // else if(Game.GuessLetterCorrectly(guess) == true)
+      // {
+      //   Game.MatchCharValidator(guess);
+      //   rightguess ++;
+      // }
+
+      
+
+      return RedirectToAction("Index", dict); 
     }
   }
 }
--- Hangman/Models/Game.cs
+++ Hangman/Models/Game.cs
@@ -1,22 +1,27 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Hangman.Models
 {
   public class Game
   {
-    public string WordToGuess { get; set; }
+    public static string WordToGuess { get; set; }
     public static char[] CharactersToGuess;
     private static List<string> _wordList = new List<string> {"test", "guess", "words"};
   
     private static List<char> _lettersGuessed; 
-    public int wrongGuesses { get; set; }
+    public static int WrongGuesses { get; set; }
+    public static int Rightguesses { get; set; }
+    public static List<int> MatchedChar = new List<int> {};
+    public static List<int> UnmatchedChar = new List<int> {};
     public Game()
     {
       WordToGuess = RandomWordGenerator();
       CharactersToGuess = WordToGuess.ToCharArray();
       _lettersGuessed = new List<char>() {};
-      wrongGuesses = 0;
+      WrongGuesses = 0;
+      Rightguesses = 0;
     }
 
     private string RandomWordGenerator()
@@ -25,6 +30,11 @@ namespace Hangman.Models
       int _wordListLength = _wordList.Count;
       int randomNumber = rand.Next(_wordListLength);
       return _wordList[randomNumber];
+    }
+
+    public static string CurrentImageLink()
+    {
+      return "Hangman" + WrongGuesses + ".png";
     }
 
     public static List<char> GetAllGuessedLetters()
@@ -39,7 +49,11 @@ namespace Hangman.Models
 
     public static bool AlreadyGuessed(char letter)
     {
-      if (_lettersGuessed.Contains(letter))
+      if (_lettersGuessed == null)
+      {
+        return false;
+      }
+      else if (_lettersGuessed.Contains(letter))
       {
         return true;
       }
@@ -50,39 +64,55 @@ namespace Hangman.Models
     }
 
 
-    public static bool GuessLetterCorrectly(char letter)
-    {
-      if (AlreadyGuessed)
-      {
-        //Give error, ask to guess another letter
-      }
-      else
-      {
-        _lettersGuessed.Add(letter);
-        if(CharactersToGuess.Contains(letter))
-        {
-          return true;
-        }
-        else
-        {
-          wrongGuesses++;
-          return false;
-        }
-      }
-      
-    }
+    // public static bool GuessLetterCorrectly(char letter)
+    // {
+    //   // if (AlreadyGuessed)
+    //   // {
+    //   //   //Give error, ask to guess another letter
+    //   // }
+    //   _lettersGuessed.Add(letter);
+    //   if(CharactersToGuess.Contains(letter) == true)
+    //   {
+    //     // RightGuesses ++;
+    //     return true;
+    //   }
+    //   else
+    //   {
+    //     WrongGuesses++;
+    //     return false;
+    //   }
+    // }
 
-    public static List<int> MatchChar(char letter)
-    {
-      List<int> matchChar = new List<int> {};
-      for(int i = 0; i < CharacterstoGuess.Length; i ++)
-      {
-        if(letter == CharacterstoGuess[i])
-        {
-          matchChar.Add(i);
-        }
-      }
-      return matchChar;
-    }
+    // public static List<int> MatchChar(char letter)
+    // {
+
+    //   // for(int i = 0; i < CharacterstoGuess.Length; i ++)
+    //   // {
+    //   //   if(letter == CharacterstoGuess[i])
+    //   //   {
+    //   //     MatchedChar.Add(i);
+    //   //   }
+    //   // }
+    //   // return MatchedChar;
+    // }
+
+    // public static void MatchCharValidator(char letter)
+    // {
+
+    //   for(int i = 0; i < CharactersToGuess.Length; i ++)
+    //   {
+    //     if(letter == CharactersToGuess[i])
+    //     {
+    //       // MatchedChar.Add(CharacterstoGuess[i]);
+    //       Rightguesses ++;
+    //     }
+    //     else
+    //     {
+    //       // UnmatchedChar.Add(CharacterstoGuess[i]);
+    //       WrongGuesses ++;
+    //     }
+    //   }
+  
+    // }
   }
 }
